03adb09ef66e409e92e21a8195122c336b097a83
Logan Bussell <36081148+lbussell@users.noreply.github.com>
2021-12-01 20:55:18 -0800

    day 1 part 2

--- Day1/SonarSweep/Program.cs
+++ Day1/SonarSweep/Program.cs
@@ -14,9 +14,13 @@ class Program
         var input = Program.ReadInput(args[0]);
 
         var sonarSweep = new SonarSweep();
-        var numberOfMeasurementIncreases = sonarSweep.RunSweep(input);
+        var numberOfMeasurementIncreases = sonarSweep.ComputeIncreases(input);
 
         Console.WriteLine("Part 1 answer: {0}", numberOfMeasurementIncreases);
+
+        var slidingWindowIncreases = sonarSweep.ComputeSlidingWindowIncreases(input);
+
+        Console.WriteLine("Part 2 answer: {0}", slidingWindowIncreases);
     }
 
     private static int[] ReadInput(string inputFile)
@@ -29,15 +33,15 @@ class Program
 
 class SonarSweep
 {
-    private int previous = int.MaxValue;
-    private int count = 0;
-
     //
     // Computes how many measurements are larger than the previous measurement.
     //
-    public int RunSweep(int[] measurements)
+    public int ComputeIncreases(int[] measurements)
     {
-        foreach(int n in measurements)
+        int previous = int.MaxValue;
+        int count = 0;
+
+        foreach (int n in measurements)
         {
             if (n > previous)
             {
@@ -48,4 +52,25 @@ class SonarSweep
 
         return count;
     }
+
+    public int ComputeSlidingWindowIncreases(int[] measurements)
+    {
+        Func<int[], int, int[][]> windowed = (array, n) =>
+        {
+            int[][] windowedArray = new int[array.Length - (n - 1)][];
+
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                windowedArray[i] = new int[n];
+                windowedArray[i][0] = array[i];
+                windowedArray[i][1] = array[i + 1];
+                windowedArray[i][2] = array[i + 2];
+            }
+
+            return windowedArray;
+        };
+
+        return ComputeIncreases(windowed(measurements, 3).Select(w => w.Sum()).ToArray());
+    }
+
 }
